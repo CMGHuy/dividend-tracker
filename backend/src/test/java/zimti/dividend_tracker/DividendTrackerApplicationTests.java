9bd34db855808f343855b4e7ce34ed340dcd5668
hcao <caominhgiahuy@gmail.com>
2025-02-09 00:04:24 +0100

    Initializing frontend and backend

---
+++ backend/src/test/java/zimti/dividend_tracker/DividendTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package zimti.dividend_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DividendTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
